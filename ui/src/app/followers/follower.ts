9f4e0ef20bbd64ddd2cd7489a524caded33743de
jchpan <jchpan@ucdavis.edu>
2022-12-04 00:52:30 -0500

    add follower and followings

---
+++ ui/src/app/followers/follower.ts
@@ -0,0 +1,4 @@
+export class Follower {
+  UserID: string;
+  FollowerID: string;
+}
---
+++ ui/src/app/followers/followers.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Follower } from './follower';
+import { FollowerServiceService } from './follower-service.service';
+
+@Component({
+  selector: 'app-followers',
+  templateUrl: './followers.component.html',
+  styleUrls: ['./followers.component.css']
+})
+export class FollowersComponent implements OnInit {
+
+  followingList: Follower[];
+  userID: string;
+  followingService: FollowerServiceService;
+  numberOfFollower: number
+
+  constructor(followingService: FollowerServiceService) {
+    this.followingService = followingService;
+    this.followingList = [];
+    this.userID = 'jp4209';
+    this.numberOfFollower = 0;
+  }
+
+  ngOnInit(): void {
+    // this.followingService.getFollowings(this.userID)
+    //   .subscribe((data) => this.setFollowingList(data));
+  }
+
+  setFollowerList(theFollower: Follower): void {
+    // for (let i = 0; i < theFollower.length; i++) {
+    // @ts-ignore
+    for (const item of theFollower) {
+      const follower = new Follower();
+      follower.FollowerID = item.FollowerID;
+      follower.UserID = item.UserID;
+      this.followingList.push(follower);
+      console.log(follower);
+      this.numberOfFollower += 1;
+    }
+
+    const btn = document.getElementById('show_follower') as HTMLButtonElement | null;
+    btn?.setAttribute('disabled', '');
+  }
+
+  onLookup(): void {
+    this.followingService.getFollowings(this.userID)
+      .subscribe((data) => this.setFollowerList(data));
+
+    const txt = document.getElementById('follower_number')
+    txt.style.display = 'block';
+  }
+}
