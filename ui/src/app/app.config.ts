527ac3f480444ee1f630e0e4a734765c7e0bf689
oliverliuoo <90397931+oliverliuoo@users.noreply.github.com>
2022-12-17 11:43:57 -0500

    Add href in following and follower list and incoporate api-gateway endpoint. (#32)
    Co-authored-by: Oliver Liu <oliverliu@Olivers-MacBook-Pro-2.local>

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -6,6 +6,7 @@ export const appProperties = {
   postServiceEndPoint: "https://lgwxyktbpd.execute-api.us-east-2.amazonaws.com/prod/",
   // commentServiceEndPoint: "http://127.0.0.1:5012/",
   commentServiceEndPoint: "https://lgwxyktbpd.execute-api.us-east-2.amazonaws.com/prod/",
-  feedServiceEndPoint: "http://socialmediafeed-env.eba-ait8ygdw.us-east-2.elasticbeanstalk.com",
+  // feedServiceEndPoint: "http://socialmediafeed-env.eba-ait8ygdw.us-east-2.elasticbeanstalk.com",
+  feedServiceEndPoint: "https://lgwxyktbpd.execute-api.us-east-2.amazonaws.com/prod/",
   photoPlaceHolderUrl: 'https://social-media-photo-bucket.s3.us-east-2.amazonaws.com/placeholder.png',
 }
