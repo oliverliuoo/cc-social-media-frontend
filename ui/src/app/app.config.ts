56cd27a5d36153e945356fc083987015139b7a3d
Oliver Liu <oliverliu@Olivers-MacBook-Pro-2.local>
2022-12-16 23:54:05 -0500

    Change userService endpoint to api-gateway.

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,7 +1,8 @@
 export const appProperties = {
   // userServiceEndPoint: "https://127.0.0.1:5011/",
-  userServiceEndPoint: "https://ec2-54-172-1-203.compute-1.amazonaws.com:5011/",
-  // postServiceEndPoint: "http://socihttps://54.172.1.203:5011/al-media-post.us-east-2.elasticbeanstalk.com/",
+  // userServiceEndPoint: "http://socialmediauserservice-env.eba-mypq7pxk.us-east-1.elasticbeanstalk.com/",
+  userServiceEndPoint: "https://lgwxyktbpd.execute-api.us-east-2.amazonaws.com/prod/user/",
+  // postServiceEndPoint: "http://https://54.172.1.203:5011/al-media-post.us-east-2.elasticbeanstalk.com/",
   postServiceEndPoint: "https://lgwxyktbpd.execute-api.us-east-2.amazonaws.com/prod/",
   // commentServiceEndPoint: "http://127.0.0.1:5012/",
   commentServiceEndPoint: "https://lgwxyktbpd.execute-api.us-east-2.amazonaws.com/prod/",
