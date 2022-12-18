c9d531d87c025e356b754330af7decc2ac7be1ef
oliverliuoo <90397931+oliverliuoo@users.noreply.github.com>
2022-12-17 20:49:36 -0500

    Add href in following and follower list and incoporate api-gateway endpoint. (#32) (#33)
    Co-authored-by: Oliver Liu <oliverliu@Olivers-MacBook-Pro-2.local>
    
    Co-authored-by: Oliver Liu <oliverliu@Olivers-MacBook-Pro-2.local>

--- ui/src/app/card/card.component.ts
+++ ui/src/app/card/card.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {DeletePostDialogComponent} from "../delete-post-dialog/delete-post-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
+import {PostPageService} from "../post-page/post-page.service";
 
 
 @Component({
@@ -10,12 +11,19 @@ import {MatDialog} from "@angular/material/dialog";
 })
 export class CardComponent implements OnInit {
 
-  constructor(  private dialogModel: MatDialog) { }
+  constructor(  private dialogModel: MatDialog,
+                private postService: PostPageService) { }
   @Input() postText: string;
   @Input() imgSrc: string | undefined;
   @Input() postId: string;
+  deleteVisible: boolean = true;
 
   ngOnInit(): void {
+    this.postService.getPostByPostId(this.postId).subscribe((postRsp) => {
+      if (postRsp.data[0].user_id !== localStorage.getItem('userId')) {
+        this.deleteVisible = false;
+      }
+    });
   }
 
   goToDetailPage(): void {
